38cd0b76ed0d0d78edeae243b0aa548debb47c3d
Michaelllllllllllllll <133153860+Michaelllllllllllllll@users.noreply.github.com>
2023-06-11 16:51:49 -0400

    ajout des planetes

--- Module/__init__.py
+++ Module/__init__.py
@@ -1,3 +1,4 @@
 from .fct_IHM import *
 from .fct_simple import *
 from .vaisseau import *
+from .planete import *
--- Module/fct_IHM.py
+++ Module/fct_IHM.py
@@ -1,4 +1,4 @@
-def afficher_ihm(obtenir_entier, obtenir_flottant, Vaisseau):
+def afficher_ihm(obtenir_entier, obtenir_flottant, Vaisseau, Planete):
     """Affiche l'interface utilisateur
 
     Input : aucune
@@ -6,6 +6,9 @@ def afficher_ihm(obtenir_entier, obtenir_flottant, Vaisseau):
     Output : dictionnaire avec les données d'entrées du problème"""
     mission = {}
     print("Voici les planètes du système solaire :\n1 - Mercure\n2 - Vénus\n3 - Terre\n4 - Mars\n5 - Jupiter\n6 - Saturne\n7 - Uranus\n8 - Neptune\n9 - Pluton")
-    mission['planete_depart'] = obtenir_entier("Veuillez entrer le numéro de la planète de départ de votre voyage : ")
-    vaisseau = Vaisseau(obtenir_flottant)
-    planete = ['Mercure', 'Vénus', 'Terre', 'Mars', 'Jupiter', 'Saturne', 'Uranus', 'Neptune', 'Pluton']
+    mission['numero_planete_depart'] = obtenir_entier("Veuillez entrer le numéro de la planète de départ de votre voyage : ") - 1
+    mission['numero_planete_arrivee'] = obtenir_entier("Veuillez entrer le numéro de la planète d'arrivée de votre voyage : ") - 1
+    mission['planete_depart'] = Planete(mission['numero_planete_depart'])
+    mission['planete_arrivee'] = Planete(mission['numero_planete_arrivee'])
+    mission['vaisseau'] = Vaisseau(obtenir_flottant)
+    return mission
