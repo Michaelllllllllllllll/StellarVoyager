ee923d9b2dfac3616770a9b40e58aa90bde7a392
Michaelllllllllllllll <133153860+Michaelllllllllllllll@users.noreply.github.com>
2023-06-11 11:26:11 -0400

    fin IHM_vaisseau

--- Module/fct_IHM.py
+++ Module/fct_IHM.py
@@ -1,4 +1,4 @@
-def afficher_ihm():
+def afficher_ihm(obtenir_entier, obtenir_flottant, Vaisseau):
     """Affiche l'interface utilisateur
 
     Input : aucune
@@ -7,29 +7,5 @@ def afficher_ihm():
     mission = {}
     print("Voici les planètes du système solaire :\n1 - Mercure\n2 - Vénus\n3 - Terre\n4 - Mars\n5 - Jupiter\n6 - Saturne\n7 - Uranus\n8 - Neptune\n9 - Pluton")
     mission['planete_depart'] = obtenir_entier("Veuillez entrer le numéro de la planète de départ de votre voyage : ")
-    vaisseau = Vaisseau()
+    vaisseau = Vaisseau(obtenir_flottant)
     planete = ['Mercure', 'Vénus', 'Terre', 'Mars', 'Jupiter', 'Saturne', 'Uranus', 'Neptune', 'Pluton']
-
-
-def obtenir_entier(question):
-    """Vérifie que l'entrée de l'utilisateur est un nombre entier.
-
-    :param question: Une chaîne de caractères contenant la question posée à l'utilisateur.
-    :type question: str
-
-    :return: Le nombre entier entré par l'utilisateur.
-    :rtype: int"""
-    while True:
-        # Vérifiez constamment si une valeur a été saisie suivie de "Try"
-        try:
-            # Mettre la valeur entrée par l'utilisateur dans "entree"
-            entree = input(question)
-            # Essayez de le transformer en nombre flottant, s'il ne peut pas, nous passons à "except"
-            valeur = float(entree)
-            if valeur % 1 != 0:
-                valeur = ''
-            valeur = int(valeur)
-            return valeur
-        except ValueError:
-            # Si l'entree n'est pas un nombre flottant, nous avons une erreur
-            print("Entrée invalide, un entier est attendu, veuillez réessayer.")
--- Module/vaisseau.py
+++ Module/vaisseau.py
@@ -6,30 +6,10 @@ class Vaisseau:
     :ivar masse_reactive: La masse réactive du carburant.
     :vartype masse_reactive: float"""
 
-    def __init__(self):
-        """Initialise un objet Vaisseau.
+    def __init__(self, obtenir_flottant):
+        """IInitialise un objet Vaisseau.
 
-        :param float masse_vide: La masse du vaisseau à vide.
-        :param float masse_reactive: La masse réactive du carburant."""
+        :param obtenir_flottant: Une fonction qui permet d'obtenir un nombre flottant de l'utilisateur.
+        :type obtenir_flottant: function"""
         self.masse_vide = obtenir_flottant('Entrer la masse à vide du vaisseau : ')
         self.masse_reactive = obtenir_flottant('Entrer la masse de carburant réactif : ')
-
-def obtenir_flottant(question):
-    """Vérifie que l'entrée de l'utilisateur est un nombre flottant.
-
-    :param question: Une chaîne de caractères contenant la question posée à l'utilisateur.
-    :type question: str
-
-    :return: Le nombre flottant entré par l'utilisateur.
-    :rtype: float"""
-    while True:
-        # Vérifiez constamment si une valeur a été saisie suivie de "Try"
-        try:
-            # Mettre la valeur entrée par l'utilisateur dans "entree"
-            entree = input(question)
-            # Essayez de le transformer en nombre flottant, s'il ne peut pas, nous passons à "except"
-            valeur = float(entree)
-            return valeur
-        except ValueError:
-            # Si l'entree n'est pas un nombre flottant, nous avons une erreur
-            print("Entrée invalide, un flottant est attendu, veuillez réessayer.")
