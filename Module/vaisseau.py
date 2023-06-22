0ccf0b72919b624a0a956db041e1dfdef238745d
Laeeeee <laegranal@gmail.com>
2023-06-23 00:15:14 +0200

    Documentation correction

--- Module/vaisseau.py
+++ Module/vaisseau.py
@@ -1,8 +1,8 @@
 class Vaisseau:
     """Classe représent un vaisseau spatial et ses caractéristiques.
 
-    :ivar float masse_vide : La masse à vide du vaisseau en kg.
-    :ivar float masse_reactive : La masse de carburant réactif en kg.
+    :ivar float masse_vide: La masse à vide du vaisseau en kg.
+    :ivar float masse_reactive: La masse de carburant réactif en kg.
 
     Méthodes:
         __init__(self, obtenir_flottant): Initialise un objet Vaisseau.
